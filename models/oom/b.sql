e834d9151aa4fc00e19f835d3fffbd93c4a4863a
David Harting <david.harting@dbtlabs.com>
2023-05-05 15:15:12 +0000

    add OOM-kill test

---
+++ models/oom/b.sql
@@ -0,0 +1,21 @@
+-- models/foo.sql
+-- models/bar.sql
+-- models/baz.sql
+
+with foo as (
+    {% for i in range(0, 100000) %}
+    select {{ i }} as id
+    {% if not loop.last %} union all {% endif %}
+    {% endfor %}
+),
+
+bar as (
+    {% for i in range(0, 100000) %}
+    select {{ i }} as id
+    {% if not loop.last %} union all {% endif %}
+    {% endfor %}
+)
+
+select * from foo
+ union all
+select * from bar
