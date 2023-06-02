d76303c38236443c10be012310927141a10ccfe5
dev@dbtlabs.com <dev@dbtlabs.com>
2023-06-02 14:05:26 +0000

    aefd

--- models/marts/intermediate/order_items.sql
+++ models/marts/intermediate/order_items.sql
@@ -24,10 +24,11 @@ select
     line_item.return_flag,
     
     line_item.line_number,
-    line_item.status_code as order_item_status_code,
+
+    line_item.status_code as order_item_status_code,  
     line_item.ship_date,
     line_item.commit_date,
-    line_item.receipt_date,
+            line_item.receipt_date ,
     line_item.ship_mode,
     line_item.extended_price,
     line_item.quantity,
@@ -51,8 +52,10 @@ select
     ){{ money() }} as net_item_sales_amount
 
 from
-    orders
-inner join line_item
+    
+orders
+  inner
+  join line_item
         on orders.order_key = line_item.order_key
 order by
     orders.order_date
